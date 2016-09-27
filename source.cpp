d27d890d6d6733d3d1929deeeeb0a97019859cb3
Jian Wei <jianwei@jianwei-deb8-64>
2016-09-27 11:19:06 -0400

    first finished version

--- source.cpp
+++ source.cpp
@@ -27,15 +27,39 @@ public:
 			st.push(split[id]);
 			id++;
 			while (id < split.size()) {
-				if (!st.top().compare("#"))
-					st.push(split[id]);
-				else {
+				if (!split[id].compare("#")) {
+					if (!st.top().compare("#")) {
+						while (!st.empty() && !st.top().compare("#")) {
+							st.pop();
+							st.pop();
+						}
+						st.push(split[id]);
+					}
+					else {
+						st.push(split[id]);
+					}
 				}
+				else {
+					st.push(split[id]);
+				}
+				id++;
 			}
 		}
+
+		if (st.size() == 1 && !st.top().compare("#"))
+			return true;
+		return false;
 	}
 };
 
 int main() {
-	string serTree{"9,3,4,#,#,1,#,#,2,#,6,#,#"};
+	// string serTree{"9,3,4,#,#,1,#,#,2,#,6,#,#"};
+	// string serTree{"1,#"};
+	// string serTree{"9,#,#,1"};
+	string serTree{"1,2,#,4,5,#,#,#,3,6,7,#,9,#,#,8,#,#,#"};
+
+	solution soln;
+	bool isValid = soln.isValidSerialization(serTree);
+
+	std::cout << std::boolalpha << isValid << std::endl;
 }
